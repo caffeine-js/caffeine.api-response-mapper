7bdb3b6ed84a187690ea148b3b6365c1fd67a557
Alan Reis <alanreisanjo@gmail.com>
2026-02-24 12:03:42 -0300

    feat: add array mapping support for entity responses

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,11 @@ import Elysia from "elysia";
 export const CaffeineResponseMapper = new Elysia().onAfterHandle(
 	({ responseValue }) => {
 		if (responseValue instanceof Entity) return Mapper.toDTO(responseValue);
+		if (
+			Array.isArray(responseValue) &&
+			responseValue.length > 0 &&
+			responseValue[0] instanceof Entity
+		)
+			return responseValue.map((item) => Mapper.toDTO(item));
 	},
 );
