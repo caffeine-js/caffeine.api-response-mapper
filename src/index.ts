9a266a2979e6f3bfaab40f5a78f9a6e9122cc4b8
Alan Reis <alanreisanjo@gmail.com>
2026-02-25 20:17:06 -0300

    feat: unwrap ElysiaCustomStatusResponse for correct entity mapping

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import { Entity, Mapper } from "@caffeine/entity";
-import Elysia from "elysia";
+import Elysia, { type ElysiaCustomStatusResponse } from "elysia";
+
+type ResponseValue = { constructor: { name: string } };
 
 export const CaffeineResponseMapper = new Elysia().onAfterHandle(
+	{ as: "global" },
 	({ responseValue }) => {
-		if (responseValue instanceof Entity) return Mapper.toDTO(responseValue);
+		if (typeof responseValue !== "object") return;
+
+		const response =
+			"constructor" in (responseValue as ResponseValue) &&
+			(responseValue as ResponseValue).constructor.name ===
+				"ElysiaCustomStatusResponse"
+				? (responseValue as ElysiaCustomStatusResponse<number, unknown>)
+						.response
+				: responseValue;
+
+		if (response instanceof Entity) return Mapper.toDTO(response);
 		if (
-			Array.isArray(responseValue) &&
-			responseValue.length > 0 &&
-			responseValue[0] instanceof Entity
+			Array.isArray(response) &&
+			response.length > 0 &&
+			response[0] instanceof Entity
 		)
-			return responseValue.map((item) => Mapper.toDTO(item));
+			return response.map((item) => Mapper.toDTO(item));
 	},
 );
